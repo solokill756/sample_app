6839c86ad891ce61a5b7ac52726cd6b3d0e078e0
DN_Naitei22-SJP_Ruby] Ho.​Sy.​Thao(T2,3,4,5,6W) <ho.sy.thao@sun-asterisk.com>
2025-07-18 13:41:30 +0700

    chapter 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,15 +10,15 @@ module ApplicationHelper
   end
 
   def user_name user_name = ""
-    user_name.empty? ? t("helper.user") : user_name
+    user_name.empty? ? t("ui.user") : user_name
   end
 
   def language_switch_link
     if I18n.locale == :en
-      link_to t(:switch_to_vietnamese), request.params.merge(locale: :vi),
+      link_to t("ui.switch_to_vietnamese"), request.params.merge(locale: :vi),
               class: "nav-link text-white"
     else
-      link_to t(:switch_to_english), request.params.merge(locale: :en),
+      link_to t("ui.switch_to_english"), request.params.merge(locale: :en),
               class: "nav-link text-white"
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_17_075131) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_21_071543) do
   create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_17_075131) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.date "birthday"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "microposts", "users"
